cff1b629c4b2f5b190903af751bd4e6ff0d60c69
Alejandro Seco <cordo_970@hotmail.com>
2023-02-28 22:00:52 -0300

    Proyecto Final CV Alejandro Seco

---
+++ js/contenido.js
@@ -0,0 +1,9 @@
+document.getElementById('button').onclick = function () {
+    console.log("Muestra el comentario con el click")
+    document.getElementById("demo").style.display= "Carlos - Gte. Cia de Seguros - 3516966666"
+}
+
+document.getElementById('button').onclick = function () {
+    console.log("Muestra el comentario con el click")
+    document.getElementById("demo1").style.display= "Rosalyn - Profesor Eduacion IT - 115548879"
+}
